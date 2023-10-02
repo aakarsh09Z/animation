92e05638170e8062fd99ba9c598002ca002977f4
aakarsh09Z <singhaakarsh54321@gmail.com>
2023-10-03 02:41:43 +0530

    added two more fish generators

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ const colors = ["red", "yellow", "lime", "orange", "teal", "yellowgreen", "purpl
 
 let fish_generator = () => {
     const x = -250;
-    const y = Math.random() * (window.screen.height - 100);
+    const y = Math.random() * (window.screen.height - 250);
 
     let fishGroup = document.createElement("div");
     fishGroup.classList.add('fish');
@@ -51,35 +51,67 @@ let fish_generator = () => {
 
 };
 
+let fish_generator2 = () => {
+    const x = -250;
+    const y = Math.random() * (window.screen.height - 250);
 
-// let fish_generator2 = () => {
-//     const x = -250;
-//     const y = Math.random() * 500;
+    let fishGroup = document.createElement("div");
+    fishGroup.classList.add('fish2');
 
-//     let fishGroup = document.createElement("div");
-//     fishGroup.classList.add('fish');
+    let tail = document.createElement("div");
+    tail.style.borderLeftColor = colors[Math.floor(Math.random() * colors.length)];
+    tail.classList.add('tail2');
+    tail.style.left = x + "px";
+    tail.style.top = y + "px";
 
-//     let tail = document.createElement("div");
-//     tail.classList.add('tail');
-//     tail.style.left = x + "px";
-//     tail.style.top = y + "px";
 
-//     let fb = document.createElement("div");
-//     fb.classList.add('fish-body');
+    let fb = document.createElement("div");
+    fb.style.borderLeftColor = colors[Math.floor(Math.random() * colors.length)];
+    fb.classList.add('fish2-body');
 
-//     let eye = document.createElement("div");
-//     eye.classList.add('eye');
+    let eye = document.createElement("div");
+    eye.classList.add('eye2');
 
-//     fb.appendChild(eye);
-//     tail.appendChild(fb);
-//     fishGroup.appendChild(tail);
+    fb.appendChild(eye);
+    tail.appendChild(fb);
+    fishGroup.appendChild(tail);
 
-//     bubble.appendChild(fishGroup);
-// };
+    box.appendChild(fishGroup);
+
+};
+let fish_generator3 = () => {
+    const x = -250;
+    const y = Math.random() * (window.screen.height - 250);
+
+    let fishGroup = document.createElement("div");
+    fishGroup.classList.add('fish3');
+
+    let tail = document.createElement("div");
+    tail.style.borderLeftColor = colors[Math.floor(Math.random() * colors.length)];
+    tail.classList.add('tail3');
+    tail.style.left = x + "px";
+    tail.style.top = y + "px";
+
+
+    let fb = document.createElement("div");
+    fb.style.borderLeftColor = colors[Math.floor(Math.random() * colors.length)];
+    fb.classList.add('fish3-body');
+
+    let eye = document.createElement("div");
+    eye.classList.add('eye3');
+
+    fb.appendChild(eye);
+    tail.appendChild(fb);
+    fishGroup.appendChild(tail);
+
+    box.appendChild(fishGroup);
+
+};
 
 setInterval(fish_generator, 1000);
-// setInterval(fish_generator2, 1000);
+setInterval(fish_generator2, 1500);
+setInterval(fish_generator3, 2000);
 
-setInterval(bubble_generator, 350);
+setInterval(bubble_generator, 250);
 
 
